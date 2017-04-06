7775c75b85dacfe44531493e0e692e3a6349109d
Evans R <evansrutoh@gmail.com>
2017-04-06 13:55:45 +0800

    Sync with production

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -9,6 +9,8 @@ export default class Home extends Component {
     // const styles = require('./Home.scss');
     // require the logo image both from client and server
     // const profImage = require('./logo.png');
+    let tinker1 = "{'Tinkered before': [('Node', 'React', 'Angular'), ('Python', 'Flask', 'Django'), ('Asp.Net', 'Asp.Net Core'), ('Php', 'Zend', 'CodeIgniter')]}";
+    let tinker2 = "{'Currently tinkering': ['Software Defined Networking', 'OpenFlow', 'Hadoop', 'Java']}";
     return (
       <div className={css.home}>
         <Helmet title="Home"/>
@@ -18,22 +20,23 @@ export default class Home extends Component {
               <div className={css.bio_row}>
                 <div className={css.bio_3_5}>
                   <p>
-                    <span className={css.block}>Lorem ipsum dolor sit amet, </span>
-                    <span className={css.block}>Distinctio necessitatibus quam</span>
-                    <span className={css.block}>Ab asperiores des </span>
-                    <span className={css.block}>Enim facilis modi obcaecati </span>
+                    <span className={css.block}>A FullStack Developer, </span>
+                    <span className={css.block}>Habitual tinkerer</span>
+                    <span className={css.block}><code>{ tinker1 }</code></span>
+                    <span className={css.block}><code>{ tinker2 }</code> </span>
                   </p>
-                  <h2>Building for the web.</h2>
+                  <h2>If something is worth building, it's worth building right..</h2>
                 </div>
                 <div className={css.bio_2_5}>
-                  <img src={require('./logo.png')} alt="An picture of me" />
+                  <img src={require('./me.jpg')} alt="An picture of me" />
                 </div>
               </div>
             </div>
             <div id="bio-description">
               <blockquote>
-                <p>Whether you need a startup dev team, or a simple website, we make it happen.</p>
-                <p>Our way of breaking it down into iterative steps makes the process of creating your product easy to comprehend and control.</p>
+                <p>Hi! I'm Evans Rutoh a China-based Software Developer from Kenya. I'm passionate about web and mobile technologies, electronic music and travels.</p>
+                <p>I can write solid and well-organized code from scratch, and I have a passion for exploring new and emerging web technologies. I ensure that my projects are optimised and responsive</p>
+                <p>If you wanna talk about the web, a new cool project or invite me to play soccer, send me an e-mail.</p>
               </blockquote>
             </div>
             <hr className={css.divider} />
