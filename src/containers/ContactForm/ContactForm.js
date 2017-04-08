b9d3f6cd456c5268ed8e76f85de3021b510c4df3
Evans R <evansrutoh@gmail.com>
2017-04-08 17:17:44 +0800

    Contact form Style updates

--- src/containers/ContactForm/ContactForm.js
+++ src/containers/ContactForm/ContactForm.js
@@ -5,24 +5,28 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+import css from './ContactForm.scss';
+
 class ContactForm extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className={css.form_item}>
           <label htmlFor="name">Name</label>
-          <Field name="name" component="input" type="text"/>
+          <Field name="name" placeholder="Name" component="input" type="text"/>
         </div>
-        <div>
+        <div className={css.form_item}>
           <label htmlFor="email">Email</label>
-          <Field name="email" component="input" type="email"/>
+          <Field name="email" placeholder="xyz@abc.com" component="input" type="email"/>
         </div>
-        <div>
+        <div className={css.form_item}>
           <label htmlFor="message">Message</label>
           <Field name="message" component="textarea" placeholder="Hello to you too!"/>
         </div>
-        <button type="submit">Submit</button>
+        <div className={css.form_item}>
+          <button type="submit">Submit</button>
+        </div>
       </form>
     );
   }
